46b9caab5f21ca64db49ea60122d370ed3d93d9b
Alexander Kutsyk <kutsik.alexander@gmail.com>
2026-04-25 16:42:53 +0300

    Lesson 2026-04-25

---
+++ Assets/Test/Scripts/CarControl.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CarControl : MonoBehaviour
+{
+    [SerializeField] float maxSpeed = 1.0f;
+    [SerializeField] float maxSteerAngle = 45.0f;
+    Vector2 direction;
+    [SerializeField] WheelCollider[] frontWheels = new WheelCollider[2];
+    [SerializeField] WheelCollider[] backWheels = new WheelCollider[2];
+
+    public void OnMove(InputAction.CallbackContext callbackContext)
+    {
+        direction = callbackContext.ReadValue<Vector2>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        foreach (WheelCollider frontWheel in frontWheels)
+        {
+            frontWheel.motorTorque = direction.y * maxSpeed;
+            frontWheel.steerAngle = direction.x * maxSteerAngle;
+        }
+        foreach (WheelCollider backWheel in backWheels)
+        {
+            backWheel.motorTorque = direction.y * maxSpeed;
+        }
+    }
+}
