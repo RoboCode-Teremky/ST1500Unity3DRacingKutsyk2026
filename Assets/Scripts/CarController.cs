4da5346b8c793f586db69aa7504833e377680eb2
Alexander Kutsyk <kutsik.alexander@gmail.com>
2026-05-09 16:48:31 +0300

    Lesson 2026-05-09

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class CarController : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class CarController : MonoBehaviour
     [SerializeField] WheelCollider[] frontWheels;
     [SerializeField] WheelCollider[] backWheels;
     Vector2 direction;
+    Rigidbody rigidbody;
+    [SerializeField] TMP_Text speedometer;
 
     public void OnMove(InputAction.CallbackContext callbackContext)
     {
@@ -16,7 +19,7 @@ public class CarController : MonoBehaviour
 
     void Start()
     {
-
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
@@ -29,6 +32,46 @@ public class CarController : MonoBehaviour
         foreach(WheelCollider wheel in backWheels)
         {
             wheel.motorTorque = direction.y * speed;
+        }
+    }
+
+    void Update()
+    {
+        speedometer.text = $"Speed: {rigidbody.linearVelocity.magnitude:F1}";
+    }
+
+    int availableNitro = 0;
+    bool isNitro = false;
+    [SerializeField] float nitroSpeed = 1000.0f;
+    [SerializeField] TMP_Text nitroLabel;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Nitro"))
+        {
+            other.gameObject.SetActive(false);
+            availableNitro++;
+            nitroLabel.text = $"Nitro: {availableNitro}";
+        }
+    }
+
+    public void OnNitro(InputAction.CallbackContext callbackContext)
+    {
+        if(callbackContext.started && availableNitro > 0)
+        {
+            availableNitro--;
+            rigidbody.AddForce(transform.forward*nitroSpeed, ForceMode.Impulse);
+            nitroLabel.text = $"Nitro: {availableNitro}";
+        }
+    }
+
+    [SerializeField] float restartHigh = 2.0f;
+    public void OnRestart(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.started)
+        {
+            transform.position = transform.position + Vector3.up*restartHigh;
+            transform.localRotation = Quaternion.identity;
         }
     }
 }
